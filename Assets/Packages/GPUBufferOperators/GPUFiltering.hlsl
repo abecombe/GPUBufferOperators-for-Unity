6451119b1f2559f1c40e22dbf1bc5a002897043d
abecombe <abecombe240@yahoo.co.jp>
2024-07-31 21:35:14 +0900

    refactoring

--- Assets/Packages/GPUBufferOperators/GPUFiltering.hlsl
+++ Assets/Packages/GPUBufferOperators/GPUFiltering.hlsl
@@ -20,16 +20,16 @@ RWStructuredBuffer<DATA_TYPE> data_out_buffer;
 RWStructuredBuffer<uint> group_sum_buffer;
 RWStructuredBuffer<uint> global_prefix_sum_buffer;
 
-int num_elements;
-int num_groups;
-int group_offset;
+uint num_elements;
+uint num_groups;
+uint group_offset;
 
-static const int num_elements_per_group = NUM_GROUP_THREADS;
-static const int log_num_elements_per_group = log2(num_elements_per_group);
-static const int num_elements_per_group_1 = num_elements_per_group - 1;
+static const uint num_elements_per_group = NUM_GROUP_THREADS;
+static const uint log_num_elements_per_group = log2(num_elements_per_group);
+static const uint num_elements_per_group_1 = num_elements_per_group - 1u;
 
-static const int s_data_len = num_elements_per_group;
-static const int s_scan_len = num_elements_per_group;
+static const uint s_data_len = num_elements_per_group;
+static const uint s_scan_len = num_elements_per_group;
 
 groupshared DATA_TYPE s_data[s_data_len];
 groupshared uint s_scan[s_scan_len];
@@ -38,10 +38,10 @@ groupshared uint s_scan[s_scan_len];
  * \brief sort input data locally and output the number of filtered elements within groups
  */
 [numthreads(NUM_GROUP_THREADS, 1, 1)]
-void RadixSortLocal(int group_thread_id : SV_GroupThreadID, int group_id : SV_GroupID)
+void RadixSortLocal(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_GroupID)
 {
     group_id += group_offset;
-    int global_id = num_elements_per_group * group_id + group_thread_id;
+    uint global_id = num_elements_per_group * group_id + group_thread_id;
 
     // extract key
     DATA_TYPE data;
@@ -53,18 +53,17 @@ void RadixSortLocal(int group_thread_id : SV_GroupThreadID, int group_id : SV_Gr
     }
 
     // build scan data
-    s_scan[group_thread_id] = key;
+    s_scan[group_thread_id] = (uint)key;
     GroupMemoryBarrierWithGroupSync();
 
     // Hillis-Steele scan
     [unroll(log_num_elements_per_group)]
-    for (int offset = 1;; offset <<= 1)
+    for (uint offset = 1u;; offset <<= 1u)
     {
         uint sum = s_scan[group_thread_id];
-        int partner = group_thread_id - offset;
-        if (partner >= 0)
+        if (group_thread_id >= offset)
         {
-            sum += s_scan[partner];
+            sum += s_scan[group_thread_id - offset];
         }
         GroupMemoryBarrierWithGroupSync();
         s_scan[group_thread_id] = sum;
@@ -73,7 +72,7 @@ void RadixSortLocal(int group_thread_id : SV_GroupThreadID, int group_id : SV_Gr
 
     uint total = s_scan[num_elements_per_group_1];
 
-    if (group_thread_id == 0)
+    if (group_thread_id == 0u)
     {
         // copy the number of filtered elements to global memory
         group_sum_buffer[group_id] = total;
@@ -81,10 +80,10 @@ void RadixSortLocal(int group_thread_id : SV_GroupThreadID, int group_id : SV_Gr
     }
 
     // sort the input data locally
-    int new_id = 0;
-    if (group_thread_id > 0)
+    uint new_id = 0u;
+    if (group_thread_id > 0u)
     {
-        new_id = s_scan[group_thread_id - 1];
+        new_id = s_scan[group_thread_id - 1u];
     }
     if (key)
     {
@@ -93,7 +92,7 @@ void RadixSortLocal(int group_thread_id : SV_GroupThreadID, int group_id : SV_Gr
     GroupMemoryBarrierWithGroupSync();
 
     // copy sorted input data to global memory
-    if ((uint)group_thread_id < total)
+    if (group_thread_id < total)
     {
         data_out_buffer[global_id] = s_data[group_thread_id];
     }
@@ -103,14 +102,14 @@ void RadixSortLocal(int group_thread_id : SV_GroupThreadID, int group_id : SV_Gr
  * \brief copy input data to final position in global memory
  */
 [numthreads(NUM_GROUP_THREADS, 1, 1)]
-void GlobalShuffle(int group_thread_id : SV_GroupThreadID, int group_id : SV_GroupID)
+void GlobalShuffle(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_GroupID)
 {
     group_id += group_offset;
-    int global_id = num_elements_per_group * group_id + group_thread_id;
+    uint global_id = num_elements_per_group * group_id + group_thread_id;
 
-    if ((uint)group_thread_id < group_sum_buffer[group_id])
+    if (group_thread_id < group_sum_buffer[group_id])
     {
-        int new_id = group_thread_id + global_prefix_sum_buffer[group_id];
+        uint new_id = group_thread_id + global_prefix_sum_buffer[group_id];
         // copy data to the final destination
         data_out_buffer[new_id] = data_in_buffer[global_id];
     }
