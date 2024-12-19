9806dc79617ae6f11c5d621db3e272df7a51c31f
abecombe <abecombe@gmail.com>
2024-12-19 23:29:49 +0900

    Add start and end index support to RadixSort

--- Assets/Packages/GPUBufferOperators/Runtime/GPURadixSort.cs
+++ Assets/Packages/GPUBufferOperators/Runtime/GPURadixSort.cs
@@ -27,6 +27,7 @@ namespace Abecombe.GPUBufferOperators
         private const int MaxDispatchSize = 65535;
 
         protected ComputeShader RadixSortCs;
+        private int _kernelComputeDispatchSize;
         private int _kernelRadixSortLocal;
         private int _kernelGlobalShuffle;
 
@@ -41,6 +42,12 @@ namespace Abecombe.GPUBufferOperators
         // buffer to store the sums of each 4bit key-value (0, 1, 2, ..., 16) within locally sorted groups
         // size: 16 * number of groups
         private GraphicsBuffer _groupSumBuffer;
+        // buffer to store the group size for dispatching
+        // size: 4 (groupSize.x, groupSize.y, groupSize.z, groupCount)
+        private GraphicsBuffer _groupSizeBuffer;
+
+        private int[] _sortStartEndIndexArray = new int[2];
+        private int[] _groupSizeArray = new int[4];
 
         private bool _inited = false;
 
@@ -52,8 +59,10 @@ namespace Abecombe.GPUBufferOperators
         private void Init()
         {
             if (!RadixSortCs) LoadComputeShader();
+            _kernelComputeDispatchSize = RadixSortCs.FindKernel("ComputeDispatchSize");
             _kernelRadixSortLocal = RadixSortCs.FindKernel("RadixSortLocal");
             _kernelGlobalShuffle = RadixSortCs.FindKernel("GlobalShuffle");
+            _groupSizeBuffer = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, 4, sizeof(uint));
 
             _inited = true;
         }
@@ -70,7 +79,30 @@ namespace Abecombe.GPUBufferOperators
         /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
         public void Sort(GraphicsBuffer dataBuffer, KeyType keyType, uint maxValue = uint.MaxValue)
         {
-            Sort(dataBuffer, SortingOrder.Ascending, keyType, maxValue);
+            Sort(dataBuffer, 0, dataBuffer.count, SortingOrder.Ascending, keyType, maxValue);
+        }
+        /// <summary>
+        /// Sort data buffer in ascending order
+        /// </summary>
+        /// <param name="dataBuffer">data buffer to be sorted</param>
+        /// <param name="startIndex">start index of the data buffer for sorting</param>
+        /// <param name="endIndex">end index of the data buffer for sorting</param>
+        /// <param name="keyType">sorting key type (uint, int or float)</param>
+        /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
+        public void Sort(GraphicsBuffer dataBuffer, int startIndex, int endIndex, KeyType keyType, uint maxValue = uint.MaxValue)
+        {
+            Sort(dataBuffer, startIndex, endIndex, SortingOrder.Ascending, keyType, maxValue);
+        }
+        /// <summary>
+        /// Sort data buffer in ascending order
+        /// </summary>
+        /// <param name="dataBuffer">data buffer to be sorted</param>
+        /// <param name="sortStartEndIndexBuffer">buffer to store the [start, end) index for sorting (buffer size: 2)</param>
+        /// <param name="keyType">sorting key type (uint, int or float)</param>
+        /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
+        public void Sort(GraphicsBuffer dataBuffer, GraphicsBuffer sortStartEndIndexBuffer, KeyType keyType, uint maxValue = uint.MaxValue)
+        {
+            Sort(dataBuffer, sortStartEndIndexBuffer, SortingOrder.Ascending, keyType, maxValue);
         }
 
         /// <summary>
@@ -81,7 +113,30 @@ namespace Abecombe.GPUBufferOperators
         /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
         public void SortDescending(GraphicsBuffer dataBuffer, KeyType keyType, uint maxValue = uint.MaxValue)
         {
-            Sort(dataBuffer, SortingOrder.Descending, keyType, maxValue);
+            Sort(dataBuffer, 0, dataBuffer.count, SortingOrder.Descending, keyType, maxValue);
+        }
+        /// <summary>
+        /// Sort data buffer in descending order
+        /// </summary>
+        /// <param name="dataBuffer">data buffer to be sorted</param>
+        /// <param name="startIndex">start index of the data buffer for sorting</param>
+        /// <param name="endIndex">end index of the data buffer for sorting</param>
+        /// <param name="keyType">sorting key type (uint, int or float)</param>
+        /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
+        public void SortDescending(GraphicsBuffer dataBuffer, int startIndex, int endIndex, KeyType keyType, uint maxValue = uint.MaxValue)
+        {
+            Sort(dataBuffer, startIndex, endIndex, SortingOrder.Descending, keyType, maxValue);
+        }
+        /// <summary>
+        /// Sort data buffer in descending order
+        /// </summary>
+        /// <param name="dataBuffer">data buffer to be sorted</param>
+        /// <param name="sortStartEndIndexBuffer">buffer to store the [start, end) index for sorting (buffer size: 2)</param>
+        /// <param name="keyType">sorting key type (uint, int or float)</param>
+        /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
+        public void SortDescending(GraphicsBuffer dataBuffer, GraphicsBuffer sortStartEndIndexBuffer, KeyType keyType, uint maxValue = uint.MaxValue)
+        {
+            Sort(dataBuffer, sortStartEndIndexBuffer, SortingOrder.Descending, keyType, maxValue);
         }
 
         /// <summary>
@@ -93,21 +148,53 @@ namespace Abecombe.GPUBufferOperators
         /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
         public void Sort(GraphicsBuffer dataBuffer, SortingOrder sortingOrder, KeyType keyType, uint maxValue = uint.MaxValue)
         {
+            Sort(dataBuffer, 0, dataBuffer.count, sortingOrder, keyType, maxValue);
+        }
+        /// <summary>
+        /// Sort data buffer in specified order
+        /// </summary>
+        /// <param name="dataBuffer">data buffer to be sorted</param>
+        /// <param name="startIndex">start index of the data buffer for sorting</param>
+        /// <param name="endIndex">end index of the data buffer for sorting</param>
+        /// <param name="sortingOrder"> sorting order (ascending or descending)</param>
+        /// <param name="keyType">sorting key type (uint, int or float)</param>
+        /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
+        public void Sort(GraphicsBuffer dataBuffer, int startIndex, int endIndex, SortingOrder sortingOrder, KeyType keyType, uint maxValue = uint.MaxValue)
+        {
             if (!_inited) Init();
 
             var cs = RadixSortCs;
             var k_local = _kernelRadixSortLocal;
             var k_shuffle = _kernelGlobalShuffle;
 
-            int numElements = dataBuffer.count;
-            int numGroups = (numElements + NumElementsPerGroup - 1) / NumElementsPerGroup;
+            int bufferSize = dataBuffer.count;
+            int maxNumGroups = (bufferSize + NumElementsPerGroup - 1) / NumElementsPerGroup;
 
-            CheckBufferSizeChanged(numElements, numGroups, dataBuffer.stride);
+            CheckBufferSizeChanged(bufferSize, maxNumGroups, dataBuffer.stride);
 
-            cs.SetInt("num_elements", numElements);
-            cs.SetInt("num_groups", numGroups);
+            cs.DisableKeyword("USE_INDIRECT_DISPATCH");
+
             cs.SetInt("key_type", (int)keyType);
             cs.SetInt("sorting_order", (int)sortingOrder);
+
+            _sortStartEndIndexArray[0] = startIndex;
+            _sortStartEndIndexArray[1] = endIndex;
+            cs.SetInts("start_end_index", _sortStartEndIndexArray);
+
+            int threadCount = endIndex - startIndex;
+            int groupCount = (threadCount + NumElementsPerGroup - 1) / NumElementsPerGroup;
+            Vector3Int groupSize = groupCount switch
+            {
+                <= MaxDispatchSize => new Vector3Int(groupCount, 1, 1),
+                <= 16 * MaxDispatchSize => new Vector3Int(16, (groupCount + 15) / 16, 1),
+                <= 128 * MaxDispatchSize => new Vector3Int(128, (groupCount + 127) / 128, 1),
+                _ => new Vector3Int(1024, (groupCount + 1023) / 1024, 1)
+            };
+            _groupSizeArray[0] = groupSize.x;
+            _groupSizeArray[1] = groupSize.y;
+            _groupSizeArray[2] = groupSize.z;
+            _groupSizeArray[3] = groupCount;
+            cs.SetInts("group_size", _groupSizeArray);
 
             cs.SetBuffer(k_local, "data_in_buffer", dataBuffer);
             cs.SetBuffer(k_local, "data_out_buffer", _tempBuffer);
@@ -119,47 +206,111 @@ namespace Abecombe.GPUBufferOperators
             cs.SetBuffer(k_shuffle, "first_index_buffer", _firstIndexBuffer);
             cs.SetBuffer(k_shuffle, "global_prefix_sum_buffer", _groupSumBuffer);
 
-            int firstBitHigh = keyType == KeyType.UInt ? Convert.ToString(maxValue, 2).Length : 32;
-            for (int bitShift = 0; bitShift < firstBitHigh; bitShift += Convert.ToString(NWay, 2).Length - 1)
+            int firstBitHigh = keyType == KeyType.UInt ? GetHighestBitPosition(maxValue) : 32;
+            for (int bitShift = 0; bitShift < firstBitHigh; bitShift += GetHighestBitPosition(NWay) - 1)
             {
                 cs.SetInt("bit_shift", bitShift);
 
                 // sort input data locally and output first-index / sums of each 4bit key-value within groups
-                for (int i = 0; i < numGroups; i += MaxDispatchSize)
-                {
-                    cs.SetInt("group_offset", i);
-                    cs.Dispatch(k_local, Mathf.Min(numGroups - i, MaxDispatchSize), 1, 1);
-                }
+                cs.Dispatch(k_local, groupSize.x, groupSize.y, groupSize.z);
 
                 // prefix scan global group sum data
                 _prefixScan.ExclusiveScan(_groupSumBuffer);
 
                 // copy input data to final position in global memory
-                for (int i = 0; i < numGroups; i += MaxDispatchSize)
-                {
-                    cs.SetInt("group_offset", i);
-                    cs.Dispatch(k_shuffle, Mathf.Min(numGroups - i, MaxDispatchSize), 1, 1);
-                }
+                cs.Dispatch(k_shuffle, groupSize.x, groupSize.y, groupSize.z);
+            }
+        }
+        /// <summary>
+        /// Sort data buffer in specified order
+        /// </summary>
+        /// <param name="dataBuffer">data buffer to be sorted</param>
+        /// <param name="sortStartEndIndexBuffer">buffer to store the [start, end) index for sorting (buffer size: 2)</param>
+        /// <param name="sortingOrder"> sorting order (ascending or descending)</param>
+        /// <param name="keyType">sorting key type (uint, int or float)</param>
+        /// <param name="maxValue">maximum key-value (valid only when keyType is UInt)</param>
+        public void Sort(GraphicsBuffer dataBuffer, GraphicsBuffer sortStartEndIndexBuffer, SortingOrder sortingOrder, KeyType keyType, uint maxValue = uint.MaxValue)
+        {
+            if (!_inited) Init();
+
+            var cs = RadixSortCs;
+            var k_dispatch = _kernelComputeDispatchSize;
+            var k_local = _kernelRadixSortLocal;
+            var k_shuffle = _kernelGlobalShuffle;
+
+            int bufferSize = dataBuffer.count;
+            int maxNumGroups = (bufferSize + NumElementsPerGroup - 1) / NumElementsPerGroup;
+
+            CheckBufferSizeChanged(bufferSize, maxNumGroups, dataBuffer.stride);
+
+            cs.EnableKeyword("USE_INDIRECT_DISPATCH");
+
+            cs.SetInt("key_type", (int)keyType);
+            cs.SetInt("sorting_order", (int)sortingOrder);
+
+            cs.SetBuffer(k_dispatch, "start_end_index_buffer", sortStartEndIndexBuffer);
+            cs.SetBuffer(k_dispatch, "group_size_buffer_write", _groupSizeBuffer);
+            cs.Dispatch(k_dispatch, 1, 1, 1);
+
+            cs.SetBuffer(k_local, "data_in_buffer", dataBuffer);
+            cs.SetBuffer(k_local, "data_out_buffer", _tempBuffer);
+            cs.SetBuffer(k_local, "start_end_index_buffer", sortStartEndIndexBuffer);
+            cs.SetBuffer(k_local, "first_index_buffer", _firstIndexBuffer);
+            cs.SetBuffer(k_local, "group_sum_buffer", _groupSumBuffer);
+            cs.SetBuffer(k_local, "group_size_buffer_read", _groupSizeBuffer);
+
+            cs.SetBuffer(k_shuffle, "data_in_buffer", _tempBuffer);
+            cs.SetBuffer(k_shuffle, "data_out_buffer", dataBuffer);
+            cs.SetBuffer(k_shuffle, "start_end_index_buffer", sortStartEndIndexBuffer);
+            cs.SetBuffer(k_shuffle, "first_index_buffer", _firstIndexBuffer);
+            cs.SetBuffer(k_shuffle, "global_prefix_sum_buffer", _groupSumBuffer);
+            cs.SetBuffer(k_shuffle, "group_size_buffer_read", _groupSizeBuffer);
+
+            int firstBitHigh = keyType == KeyType.UInt ? GetHighestBitPosition(maxValue) : 32;
+            for (int bitShift = 0; bitShift < firstBitHigh; bitShift += GetHighestBitPosition(NWay) - 1)
+            {
+                cs.SetInt("bit_shift", bitShift);
+
+                // sort input data locally and output first-index / sums of each 4bit key-value within groups
+                cs.DispatchIndirect(k_local, _groupSizeBuffer);
+
+                // prefix scan global group sum data
+                _prefixScan.ExclusiveScan(_groupSumBuffer);
+
+                // copy input data to final position in global memory
+                cs.DispatchIndirect(k_shuffle, _groupSizeBuffer);
             }
         }
 
-        private void CheckBufferSizeChanged(int numElements, int numGroups, int bufferStride)
+        private void CheckBufferSizeChanged(int bufferSize, int maxNumGroups, int bufferStride)
         {
-            if (_tempBuffer is null || _tempBuffer.count < numElements || _tempBuffer.stride != bufferStride)
+            if (_tempBuffer is null || _tempBuffer.count < bufferSize || _tempBuffer.stride != bufferStride)
             {
                 _tempBuffer?.Release();
-                _tempBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, numElements, bufferStride);
+                _tempBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, bufferSize, bufferStride);
             }
-            if (_firstIndexBuffer is null || _firstIndexBuffer.count < NWay * numGroups)
+            if (_firstIndexBuffer is null || _firstIndexBuffer.count < NWay * maxNumGroups)
             {
                 _firstIndexBuffer?.Release();
-                _firstIndexBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, NWay * numGroups, sizeof(uint));
+                _firstIndexBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, NWay * maxNumGroups, sizeof(uint));
             }
-            if (_groupSumBuffer is null || _groupSumBuffer.count != NWay * numGroups)
+            if (_groupSumBuffer is null || _groupSumBuffer.count < NWay * maxNumGroups)
             {
                 _groupSumBuffer?.Release();
-                _groupSumBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, NWay * numGroups, sizeof(uint));
+                _groupSumBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, NWay * maxNumGroups, sizeof(uint));
             }
+        }
+
+        private static int GetHighestBitPosition(uint value)
+        {
+            int position = 0;
+            while (value != 0)
+            {
+                value >>= 1;
+                position++;
+            }
+
+            return position;
         }
 
         /// <summary>
@@ -172,6 +323,7 @@ namespace Abecombe.GPUBufferOperators
             if (_tempBuffer is not null) { _tempBuffer.Release(); _tempBuffer = null; }
             if (_firstIndexBuffer is not null) { _firstIndexBuffer.Release(); _firstIndexBuffer = null; }
             if (_groupSumBuffer is not null) { _groupSumBuffer.Release(); _groupSumBuffer = null; }
+            if (_groupSizeBuffer is not null) { _groupSizeBuffer.Release(); _groupSizeBuffer = null; }
 
             _prefixScan.Dispose();
 
--- Assets/Packages/GPUBufferOperators/Runtime/GPURadixSort.hlsl
+++ Assets/Packages/GPUBufferOperators/Runtime/GPURadixSort.hlsl
@@ -1,8 +1,11 @@
 ﻿#ifndef CS_GPU_RADIX_SORT_HLSL
 #define CS_GPU_RADIX_SORT_HLSL
 
+//#pragma kernel ComputeDispatchSize
 //#pragma kernel RadixSortLocal
 //#pragma kernel GlobalShuffle
+
+//#pragma multi_compile _ USE_INDIRECT_DISPATCH
 
 // default define
 #ifndef DATA_TYPE
@@ -13,17 +16,26 @@
 #endif
 
 #define NUM_GROUP_THREADS 128
+#define MAX_DISPATCH_SIZE 65535
 
 StructuredBuffer<DATA_TYPE> data_in_buffer;
 RWStructuredBuffer<DATA_TYPE> data_out_buffer;
+
+StructuredBuffer<uint2> start_end_index_buffer;
 
 RWStructuredBuffer<uint> first_index_buffer;
 RWStructuredBuffer<uint> group_sum_buffer;
 StructuredBuffer<uint> global_prefix_sum_buffer;
 
-uint num_elements;
-uint num_groups;
-uint group_offset;
+StructuredBuffer<uint4> group_size_buffer_read;
+RWStructuredBuffer<uint4> group_size_buffer_write;
+
+#if defined(USE_INDIRECT_DISPATCH)
+#else
+uint2 start_end_index;
+uint4 group_size;
+#endif
+
 uint bit_shift;
 uint key_type;
 uint sorting_order;
@@ -91,18 +103,58 @@ inline uint4 build_s_scan_data(uint key_4_bit)
 }
 
 /**
+ * \brief compute dispatch size
+ */
+[numthreads(1, 1, 1)]
+void ComputeDispatchSize()
+{
+#if defined(USE_INDIRECT_DISPATCH)
+    uint2 start_end_index = start_end_index_buffer[0];
+#endif
+    uint thread_count = start_end_index.y - start_end_index.x;
+    uint group_count = (thread_count + num_elements_per_group_minus_1) >> log_num_elements_per_group;
+
+    uint4 group_size;
+    if (group_count <= MAX_DISPATCH_SIZE)
+    {
+        group_size = uint4(group_count, 1u, 1u, group_count);
+    }
+    else if (group_count <= 16u * MAX_DISPATCH_SIZE)
+    {
+        group_size = uint4(16u, (group_count + 15u) >> 4, 1u, group_count);
+    }
+    else if (group_count <= 64u * MAX_DISPATCH_SIZE)
+    {
+        group_size = uint4(128u, (group_count + 127u) >> 7, 1u, group_count);
+    }
+    else
+    {
+        group_size = uint4(1024u, (group_count + 1023u) >> 10, 1u, group_count);
+    }
+
+    group_size_buffer_write[0] = group_size;
+}
+
+/**
  * \brief sort input data locally and output first-index / sums of each 4bit key-value within groups
  */
 [numthreads(NUM_GROUP_THREADS, 1, 1)]
-void RadixSortLocal(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_GroupID)
+void RadixSortLocal(uint group_thread_id : SV_GroupThreadID, uint3 group_id : SV_GroupID)
 {
-    group_id += group_offset;
-    uint global_id = num_elements_per_group * group_id + group_thread_id;
+#if defined(USE_INDIRECT_DISPATCH)
+    uint4 group_size = group_size_buffer_read[0];
+#endif
+    uint group_index = group_id.x + (group_id.y + group_id.z * group_size.y) * group_size.x;
+
+#if defined(USE_INDIRECT_DISPATCH)
+    uint2 start_end_index = start_end_index_buffer[0];
+#endif
+    uint global_id = num_elements_per_group * group_index + group_thread_id + start_end_index.x;
 
     // extract 4 bits
     DATA_TYPE data = (DATA_TYPE)0;
     uint key_4_bit = n_way_1;
-    if (global_id < num_elements)
+    if (global_id < start_end_index.y)
     {
         data = data_in_buffer[global_id];
         key_4_bit = get_key_4_bit(data);
@@ -137,12 +189,12 @@ void RadixSortLocal(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_
         run_sum += get_value_in_uint16(total, i);
     }
 
-    if (group_thread_id < n_way)
+    if (group_thread_id < n_way && group_index < group_size.w)
     {
         // copy sums of each 4bit key-value to global memory
-        group_sum_buffer[group_thread_id * num_groups + group_id] = get_value_in_uint16(total, group_thread_id);
+        group_sum_buffer[group_thread_id * group_size.w + group_index] = get_value_in_uint16(total, group_thread_id);
         // copy first index of each 4bit key-value to global memory
-        first_index_buffer[group_thread_id + n_way * group_id] = get_value_in_uint16(first_index, group_thread_id);
+        first_index_buffer[group_thread_id + n_way * group_index] = get_value_in_uint16(first_index, group_thread_id);
     }
 
     // sort the input data locally
@@ -155,7 +207,7 @@ void RadixSortLocal(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_
     GroupMemoryBarrierWithGroupSync();
 
     // copy sorted input data to global memory
-    if (global_id < num_elements)
+    if (global_id < start_end_index.y)
     {
         data_out_buffer[global_id] = s_data[group_thread_id];
     }
@@ -165,27 +217,34 @@ void RadixSortLocal(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_
  * \brief copy input data to final position in global memory
  */
 [numthreads(NUM_GROUP_THREADS, 1, 1)]
-void GlobalShuffle(uint group_thread_id : SV_GroupThreadID, uint group_id : SV_GroupID)
+void GlobalShuffle(uint group_thread_id : SV_GroupThreadID, uint3 group_id : SV_GroupID)
 {
-    group_id += group_offset;
-    uint global_id = num_elements_per_group * group_id + group_thread_id;
+#if defined(USE_INDIRECT_DISPATCH)
+    uint4 group_size = group_size_buffer_read[0];
+#endif
+    uint group_index = group_id.x + (group_id.y + group_id.z * group_size.y) * group_size.x;
 
-    if (group_thread_id < n_way)
+#if defined(USE_INDIRECT_DISPATCH)
+    uint2 start_end_index = start_end_index_buffer[0];
+#endif
+    uint global_id = num_elements_per_group * group_index + group_thread_id + start_end_index.x;
+
+    if (group_thread_id < n_way && group_index < group_size.w)
     {
         // set global destination of each 4bit key-value
-        s_Pd[group_thread_id] = global_prefix_sum_buffer[group_thread_id * num_groups + group_id];
+        s_Pd[group_thread_id] = global_prefix_sum_buffer[group_thread_id * group_size.w + group_index];
         // subtract the first index of each 4bit key-value
         // to make it easier to calculate the final destination of individual data
-        s_Pd[group_thread_id] -= first_index_buffer[group_thread_id + n_way * group_id];
+        s_Pd[group_thread_id] -= first_index_buffer[group_thread_id + n_way * group_index];
     }
     GroupMemoryBarrierWithGroupSync();
 
-    if (global_id < num_elements)
+    if (global_id < start_end_index.y)
     {
         DATA_TYPE data = data_in_buffer[global_id];
         uint key_4_bit = get_key_4_bit(data);
 
-        uint new_id = group_thread_id + s_Pd[key_4_bit];
+        uint new_id = group_thread_id + s_Pd[key_4_bit] + start_end_index.x;
 
         // copy data to the final destination
         data_out_buffer[new_id] = data;
